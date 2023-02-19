0c63e14083bdd414c01721a91d95f7f868858f55
msa8wsy <msa8wsy@virginia.edu>
2023-02-18 19:56:12 -0500

    More configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+ 
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
+  }
 };
 
-export default config;
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
